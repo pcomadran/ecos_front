38dac633c5b76d746c0aadb3c01efe5b765a50af
Miguel Ortiz <ortizmichel390@gmail.com>
2024-06-27 14:03:14 -0300

    Initial project setup with React, Vite, and TypeScript, cleaned for production

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        textAlign: "center",
+        marginTop: -60,
+      }}
+    >
+      <h1>Hola equipo,</h1>
+      <h3>
+        Este es el proyecto Front-End de nuestro proyecto ECOS (Quinto Impacto),
+      </h3>
+      <h1>!! BUEN TRABAJO ¡¡</h1>
+    </div>
+  );
+}
+
+export default App;
