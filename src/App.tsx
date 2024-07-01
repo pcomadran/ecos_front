ce404d388b11e490045c4b2b9a5a73172c27b0f5
Miguel Ortiz <ortizmichel390@gmail.com>
2024-07-01 18:41:00 -0300

    Add react-router-dom and set navigation in navigation bar and App.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LandingPage from "./pages/landinPage";
 
 function App() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <Navbar />
-      <LandingPage />
-    </div>
+    <Router>
+      <div
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/proveedores" element={<LandingPage />} />
+          <Route path="/publicaciones" element={<LandingPage />} />
+          <Route path="/iniciar-sesion" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
