21643c1b391a63e1759be8aac5145885cf11f324
Miguel Ortiz <ortizmichel390@gmail.com>
2024-07-17 14:49:47 -0300

    reconfigure axios and establish backend connection

--- src/pages/landinPage.tsx
+++ src/pages/landinPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   TextField,
   Typography,
@@ -11,8 +12,50 @@ import BackgroundImage from "../../public/images/Imagen landing.png";
 import CategoriesPage from "../pages/CategoriesPage";
 import SupplierLanding from "../components/supplierLanding";
 import PublicationsLanding from "../components/PublicationsLanding";
+import SupplierCard from "../components/SupplierCard";
+import { getProductsByLetter } from "../servises/callsApi";
 
 const LandingPage = () => {
+  const [products, setProducts] = useState<any[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        if (searchTerm) {
+          const productsData = await getProductsByLetter(searchTerm);
+          console.log("Products fetched:", productsData);
+          if (Array.isArray(productsData)) {
+            setProducts(productsData);
+          } else {
+            setProducts([]);
+          }
+        } else {
+          setProducts([]);
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, [searchTerm]);
+
+  useEffect(() => {
+    console.log(
+      "Filtering products:",
+      products,
+      "with search term:",
+      searchTerm
+    );
+    setFilteredProducts(
+      products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  }, [searchTerm, products]);
+
   return (
     <div>
       <div
@@ -37,7 +80,6 @@ const LandingPage = () => {
             backgroundColor: "rgba(0, 0, 0, 0.5)",
           }}
         />
-
         <Container
           style={{
             position: "relative",
@@ -52,7 +94,6 @@ const LandingPage = () => {
             paddingTop: "24px",
           }}
         >
-          {/* Campo de búsqueda */}
           <TextField
             variant="outlined"
             placeholder="Buscar Proveedores"
@@ -77,10 +118,45 @@ const LandingPage = () => {
                 </InputAdornment>
               ),
             }}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
+          {searchTerm && (
+            <Container
+              sx={{
+                zIndex: 2,
+                position: "relative",
+                marginTop: 4,
+                backgroundColor: "#fafafa", // Fondo blanco con opacidad
+                padding: "16px",
+                borderRadius: "8px",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Sombra para contraste
+              }}
+            >
+              <Typography variant="h5" gutterBottom>
+                Resultados de tu búsqueda
+              </Typography>
+              <Grid container spacing={2} justifyContent="center">
+                {filteredProducts.map((product, index) => (
+                  <Grid
+                    item
+                    key={index}
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <SupplierCard product={product} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
+          )}
           <Grid container justifyContent="center">
             <Grid item xs={12} sm={12} sx={{ mt: 3 }}>
-              {/* Agregando el nuevo Typography con los estilos especificados */}
               <Typography
                 gutterBottom
                 sx={{
@@ -110,44 +186,44 @@ const LandingPage = () => {
         </Container>
       </div>
       <Box
-      sx={{
-        mr: 2,
-        ml: 2,
-        mb: 8,
-        mt: 3,
-        padding: "7px",
-        textAlign: "center",
-        borderTop: "1px solid #4E169D",
-        borderBottom: "1px solid #4E169D",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center", // Centra los elementos verticalmente
-      }}
-    >
-      <Typography
-        variant="body1"
         sx={{
-          maxWidth: "260px",
-          height: "48px",
-          fontSize: "22px",
-          fontWeight: 700,
-          lineHeight: "25px",
+          mr: 2,
+          ml: 2,
+          mb: 8,
+          mt: 3,
+          padding: "7px",
           textAlign: "center",
-          color: "#4E169D",
-          paddingLeft: "18px",
+          borderTop: "1px solid #4E169D",
+          borderBottom: "1px solid #4E169D",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        ¿Qué son las empresas de impacto?
-      </Typography>
-      <Typography
-        variant="body1"
-        style={{ fontWeight: 500, textAlign: "center", color: "#222222" }}
-      >
-        Son organizaciones con un compromiso fundamental con la generación de
-        un impacto positivo en la sociedad y el medio ambiente como parte
-        integral de su modelo de negocio.
-      </Typography>
-    </Box>
+        <Typography
+          variant="body1"
+          sx={{
+            maxWidth: "260px",
+            height: "48px",
+            fontSize: "22px",
+            fontWeight: 700,
+            lineHeight: "25px",
+            textAlign: "center",
+            color: "#4E169D",
+            paddingLeft: "18px",
+          }}
+        >
+          ¿Qué son las empresas de impacto?
+        </Typography>
+        <Typography
+          variant="body1"
+          style={{ fontWeight: 500, textAlign: "center", color: "#222222" }}
+        >
+          Son organizaciones con un compromiso fundamental con la generación de
+          un impacto positivo en la sociedad y el medio ambiente como parte
+          integral de su modelo de negocio.
+        </Typography>
+      </Box>
       <SupplierLanding />
       <CategoriesPage />
       <PublicationsLanding />
