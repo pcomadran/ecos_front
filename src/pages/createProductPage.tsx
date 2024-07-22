ce5626dfb0ea8e342d4d8324fe356d74e1e1403a
soliz.gaston <gaston.e.soliz@gmail.com>
2024-07-21 23:00:37 -0300

    all properties controlled excluding images

--- src/pages/createProductPage.tsx
+++ src/pages/createProductPage.tsx
@@ -8,10 +8,12 @@ import {
   MenuItem,
   OutlinedInput,
   Select,
+  TextField,
   Typography,
 } from "@mui/material";
 import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const categories = [
   "Bienestar",
@@ -69,26 +71,73 @@ const theme = createTheme({
 });
 
 export default function CreateProductPage() {
+  const [count, setCount] = useState(0);
+  const [countD, setCountD] = useState(0);
+  // const [imageUrls, setImageUrls] = useState([]);
   const {
     register,
+    setValue,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
+
+  const handleCount = (event: any) => {
+    const word = event.target.value;
+    setCount(word.length);
+  };
+
+  const handleCountD = (event: any) => {
+    const word = event.target.value;
+    setCountD(word.length);
+  };
+
+  // const handlePhotos = (event: any) => {
+  //   if (watch("imageUrls").length < 3) {
+  //     const newFiles = Array.from(event.target.files);
+  //     const existingFiles = watch("imageUrls") || [];
+  //     const totalFiles = existingFiles.concat(newFiles);
+  //     setImageUrls(totalFiles);
+  //     setValue("imageUrls", totalFiles);
+  //   }
+  // };
+
+  console.log(watch("imageUrls"));
+  // console.log("cantidad:", watch("imageUrls").length);
+
+  // console.log(errors);
 
   const isSubmit = (data: any) => console.log(data);
 
   return (
-    <Box sx={{ marginTop: "56px" }}>
+    <Box
+      sx={{ marginTop: "100px", padding: "0px 15px 60px", textAlign: "center" }}
+    >
       <Typography variant="h4" sx={{ fontSize: "25px", fontWeight: "600" }}>
         Carga de Producto/Servicio
       </Typography>
-      <Typography sx={{ fontSize: "20px", fontWeight: "500" }}>
-        Completá el formulario para subir tu Producto/Servicio{" "}
+      <Typography
+        sx={{
+          fontSize: "20px",
+          fontWeight: "500",
+          marginTop: "25px",
+          padding: "0 20px",
+        }}
+      >
+        Completá el formulario para subir tu Producto/Servicio
       </Typography>
-      <form onSubmit={handleSubmit(isSubmit)}>
+      <form
+        onSubmit={handleSubmit(isSubmit)}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          marginTop: "25px",
+        }}
+      >
         <FormControl
           sx={{
-            marginTop: "10px",
+            width: "100%",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -103,21 +152,24 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
           }}
           variant="outlined"
+          error={errors.name ? true : false}
         >
           <InputLabel required>Nombre de la organizacion</InputLabel>
           <OutlinedInput
-            {...register("name")}
+            {...register("name", { required: true })}
             label="Nombre de la organizacion"
           />
-          <FormHelperText>
+          <FormHelperText sx={{ color: "#222222" }}>
             Se visualizará en el título de la publicación
           </FormHelperText>
         </FormControl>
         <FormControl
           sx={{
-            marginTop: "10px",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -132,33 +184,36 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
           }}
           variant="outlined"
+          error={errors.short_description ? true : false}
         >
           <InputLabel required>
             Breve descripción del Producto/Servicio
           </InputLabel>
           <OutlinedInput
-            {...register("short_description")}
+            {...register("short_description", {
+              required: true,
+              maxLength: 50,
+            })}
             label="Breve descripción del Producto/Servicio"
+            onChange={handleCount}
           />
-          <FormHelperText>
-            Se visualizará en el subtítulo de la publicación 0/50{" "}
+          <FormHelperText sx={{ color: "#222222" }}>
+            Se visualizará en el subtítulo de la publicación {count}/50
           </FormHelperText>
         </FormControl>
-        <FormControl sx={{ width: "100%" }}>
-          <InputLabel>Categoría*</InputLabel>
-          <Select {...register("category")} fullWidth defaultValue="">
-            {categories.map((option) => (
-              <MenuItem key={option} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl
+        <TextField
+          {...register("category", { required: true })}
+          error={errors.category ? true : false}
+          select
+          label="Categoría"
+          defaultValue=""
+          helperText="Seleccioná la categoría de tu Producto/Servicio"
           sx={{
-            marginTop: "10px",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -173,18 +228,60 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
+            "& .MuiSelect-select": {
+              textAlign: "left",
+            },
+            "& .MuiFormHelperText-root": {
+              color: "#222222",
+            },
+          }}
+        >
+          {categories.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+        <FormControl
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#222222",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#222222",
+              },
+            },
+            "& .MuiInputLabel-root": {
+              color: "#222222",
+            },
+            "& .MuiInputLabel-root.MuiFormLabel-filled": {
+              color: "#4E169D",
+            },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
           }}
           variant="outlined"
+          error={errors.email ? true : false}
         >
           <InputLabel required>Correo Electrónico</InputLabel>
-          <OutlinedInput {...register("email")} label="Correo Electrónico" />
-          <FormHelperText>
+          <OutlinedInput
+            {...register("email", {
+              required: true,
+              pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+            })}
+            label="Correo Electrónico"
+          />
+          <FormHelperText sx={{ color: "#222222" }}>
             El mismo con el que te registraste o uno diferente
           </FormHelperText>
         </FormControl>
         <FormControl
           sx={{
-            marginTop: "10px",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -199,21 +296,24 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
           }}
           variant="outlined"
+          error={errors.email ? true : false}
         >
           <InputLabel required>Teléfono o Whatsapp</InputLabel>
           <OutlinedInput
-            {...register("phoneNumber")}
+            {...register("phoneNumber", { required: true, pattern: /^\+\d+$/ })}
             label="Teléfono o Whatsapp"
           />
-          <FormHelperText>
+          <FormHelperText sx={{ color: "#222222" }}>
             Con el siguiente formato +54 9 261 002 002
           </FormHelperText>
         </FormControl>
         <FormControl
           sx={{
-            marginTop: "10px",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -228,16 +328,27 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
           }}
           variant="outlined"
+          error={errors.instagram ? true : false}
         >
           <InputLabel>Instagram</InputLabel>
-          <OutlinedInput {...register("instagram")} label="Facebook" />
-          <FormHelperText>Podés pegar el link de tu perfil </FormHelperText>
+          <OutlinedInput
+            {...register("instagram", {
+              pattern:
+                /^https?:\/\/(www\.)?instagram\.com\/[a-zA-Z0-9._-]+\/?$/,
+            })}
+            label="Instagram"
+          />
+          <FormHelperText sx={{ color: "#222222" }}>
+            Podés pegar el link de tu perfil{" "}
+          </FormHelperText>
         </FormControl>
         <FormControl
           sx={{
-            marginTop: "10px",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -252,14 +363,64 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
           }}
           variant="outlined"
+          error={errors.facebook ? true : false}
         >
           <InputLabel>Facebook</InputLabel>
-          <OutlinedInput {...register("facebook")} label="Facebook" />
-          <FormHelperText>Podés pegar el link de tu perfil </FormHelperText>
+          <OutlinedInput
+            {...register("facebook", {
+              pattern: /^https?:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9.]+\/?$/,
+            })}
+            label="Facebook"
+          />
+          <FormHelperText sx={{ color: "#222222" }}>
+            Podés pegar el link de tu perfil{" "}
+          </FormHelperText>
         </FormControl>
-        <FormControl sx={{ width: "100%" }}>
+        <TextField
+          {...register("country", { required: true })}
+          error={errors.country ? true : false}
+          select
+          label="País*"
+          defaultValue=""
+          helperText="Seleccioná un país de la lista"
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#222222",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#222222",
+              },
+            },
+            "& .MuiInputLabel-root": {
+              color: "#222222",
+            },
+            "& .MuiInputLabel-root.MuiFormLabel-filled": {
+              color: "#4E169D",
+            },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
+            "& .MuiSelect-select": {
+              textAlign: "left",
+            },
+            "& .MuiFormHelperText-root": {
+              color: "#222222",
+            },
+          }}
+        >
+          {countries.map((option, index) => (
+            <MenuItem key={index} value={option.name}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </TextField>
+        {/* <FormControl sx={{ width: "100%" }}>
           <InputLabel>País*</InputLabel>
           <Select {...register("countries")} fullWidth defaultValue="">
             {countries.map((option, index) => (
@@ -268,8 +429,8 @@ export default function CreateProductPage() {
               </MenuItem>
             ))}
           </Select>
-        </FormControl>
-        <FormControl sx={{ width: "100%" }}>
+        </FormControl> */}
+        {/* <FormControl sx={{ width: "100%" }}>
           <InputLabel>Provincia/Estado*</InputLabel>
           <Select {...register("province")} fullWidth defaultValue="">
             {countries.map((option, index) => (
@@ -278,10 +439,15 @@ export default function CreateProductPage() {
               </MenuItem>
             ))}
           </Select>
-        </FormControl>
-        <FormControl
+        </FormControl> */}
+        <TextField
+          {...register("province", { required: true })}
+          error={errors.province ? true : false}
+          select
+          label="Provincia/Estado*"
+          defaultValue=""
+          helperText="Seleccioná una provincia/estado de la lista"
           sx={{
-            marginTop: "10px",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -296,16 +462,25 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
+            "& .MuiSelect-select": {
+              textAlign: "left",
+            },
+            "& .MuiFormHelperText-root": {
+              color: "#222222",
+            },
           }}
-          variant="outlined"
         >
-          <InputLabel>Ciudad</InputLabel>
-          <OutlinedInput {...register("city")} label="Ciudad" />
-          <FormHelperText>Sin abreviaturas, nombre completo</FormHelperText>
-        </FormControl>
+          {countries.map((option, index) => (
+            <MenuItem key={index} value={option.name}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </TextField>
         <FormControl
           sx={{
-            marginTop: "10px",
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
                 borderColor: "#222222",
@@ -320,39 +495,130 @@ export default function CreateProductPage() {
             "& .MuiInputLabel-root.MuiFormLabel-filled": {
               color: "#4E169D",
             },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
           }}
           variant="outlined"
+          error={errors.city ? true : false}
+        >
+          <InputLabel>Ciudad*</InputLabel>
+          <OutlinedInput
+            {...register("city", {
+              required: true,
+              pattern: /^[a-zA-ZÀ-ÿ\s'-]+$/,
+            })}
+            label="Ciudad*"
+          />
+          <FormHelperText sx={{ color: "#222222" }}>
+            Sin abreviaturas, nombre completo
+          </FormHelperText>
+        </FormControl>
+        <FormControl
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#222222",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#222222",
+              },
+            },
+            "& .MuiInputLabel-root": {
+              color: "#222222",
+            },
+            "& .MuiInputLabel-root.MuiFormLabel-filled": {
+              color: "#4E169D",
+            },
+            "& .MuiInputLabel-root.Mui-error": {
+              color: "red",
+            },
+          }}
+          variant="outlined"
+          error={errors.large_description ? true : false}
         >
           <InputLabel>Descripción del Producto/Servicio</InputLabel>
           <OutlinedInput
-            {...register("large_description")}
+            {...register("large_description", { maxLength: 3 })}
             label="Descripción del Producto/Servicio"
+            onChange={handleCountD}
+            multiline
+            rows={8}
+            sx={{
+              height: "192px",
+              paddingTop: "30px",
+              paddingRight: "35px",
+              overflow: "auto",
+              fontSize: "16px",
+            }}
           />
-          <FormHelperText>Máximo 300 caracteres </FormHelperText>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <FormHelperText sx={{ color: "#222222" }}>
+              Máximo 300 caracteres{" "}
+            </FormHelperText>
+            <FormHelperText sx={{ color: "#222222" }}>
+              {countD}/300
+            </FormHelperText>
+          </Box>
         </FormControl>
-        <Button
-          variant="contained"
-          component="label"
+        {/* {imageUrls.length > 0 && (
+          <Box>
+            {imageUrls.map((file, index) => (
+              <Box key={index} sx={{ marginTop: "10px" }}>
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt={`preview ${index}`}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    objectFit: "cover",
+                  }}
+                />
+              </Box>
+            ))}
+          </Box>
+        )} */}
+        <FormControl
           sx={{
-            marginTop: "10px",
-            padding: "10px 16px",
-            borderRadius: "100px",
-            width: "152px",
-            height: "40px",
-            textTransform: "none",
-            color: "#fafafa",
-            fontWeight: "bold",
-            background: "#4E169D",
-            gap: "8px",
+            textAlign: "left",
+            marginLeft: "auto",
+            marginBottom: "20px",
           }}
         >
-          <FileUploadOutlinedIcon />
-          Subir Imágen
-          <input type="file" hidden accept="image/*" multiple />
-        </Button>
-        <Typography>*Requerida al menos una imagen</Typography>
-        <Typography>Hasta 3 imágenes. </Typography>
-        <Typography>Máximo 3Mb cada una</Typography>
+          <Button
+            variant="contained"
+            component="label"
+            sx={{
+              marginTop: "10px",
+              marginBottom: "5px",
+              padding: "10px 16px",
+              borderRadius: "100px",
+              width: "152px",
+              height: "40px",
+              textTransform: "none",
+              color: "#fafafa",
+              fontWeight: "bold",
+              background: "#4E169D",
+              gap: "8px",
+            }}
+            // disabled={watch("imageUrls").length >= 3}
+          >
+            <FileUploadOutlinedIcon />
+            Subir Imágen
+            <input
+              type="file"
+              hidden
+              accept="image/*"
+              multiple
+              // onChange={handlePhotos}
+            />
+          </Button>
+          <Typography variant="caption">
+            *Requerida al menos una imagen
+          </Typography>
+          <Typography variant="caption">Hasta 3 imágenes. </Typography>
+          <Typography variant="caption">Máximo 3Mb cada una</Typography>
+        </FormControl>
         <Button
           type="submit"
           sx={{
