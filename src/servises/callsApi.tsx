a560e383f8a3c20d189bfef27fee5de816099255
soliz.gaston <gaston.e.soliz@gmail.com>
2024-07-25 21:45:14 -0300

    adding gets from the backend

--- src/servises/callsApi.tsx
+++ src/servises/callsApi.tsx
@@ -7,7 +7,7 @@ export const getAllProducts = async (): Promise<any[]> => {
   } catch (error) {
     console.error("Error fetching products:", error);
     // Retornando un array vacío en caso de error o de no encontrar productos
-    return []; 
+    return [];
   }
 };
 
@@ -19,5 +19,32 @@ export const getProductsByLetter = async (letter: string): Promise<any[]> => {
   } catch (error) {
     console.error("Error fetching products:", error);
     throw error;
+  }
+};
+
+export const getAllCategories = async () => {
+  try {
+    const response = await axios.get("/api/categories");
+    return response.data;
+  } catch (error) {
+    return [];
+  }
+};
+
+export const getAllCountries = async () => {
+  try {
+    const response = await axios.get("/api/countries");
+    return response.data;
+  } catch (error) {
+    return [];
+  }
+};
+
+export const getAllProvinces = async (countryId: number) => {
+  try {
+    const response = await axios.get(`/api/provinces/country/${countryId}`);
+    return response.data;
+  } catch (error) {
+    return [];
   }
 };
