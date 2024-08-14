4c4a088dda3e11d8123dd4f8eedad25f2952fda1
comadran.pablo <pablocomadran@gmail.com>
2024-08-14 00:17:19 -0300

    Update of the publications component and fixes to the navbar component

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import Carrusel from "./Carrusel";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -17,42 +22,137 @@ interface PublicationProps {
 const Publication: React.FC<PublicationProps> = ({
   title,
   imageUrls,
-  borderRadius,
+  borderRadius = "16px", // Valor por defecto
   date,
   text,
 }) => {
   const [showFullText, setShowFullText] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const location = useLocation();
 
   const handleToggleText = () => {
     setShowFullText((prevShowFullText) => !prevShowFullText);
   };
 
+  const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   const shortText = text.split(".")[0] + ".";
   const fullText = text.split("\n\n").join("\n\n");
+
+  // Configura aquí las rutas donde se debe mostrar el botón
+  const shouldShowMoreOptionsButton = ["/publicationsform"].includes(
+    location.pathname
+  );
+
+  // Alineación del título según la ruta
+  const titleAlignment = shouldShowMoreOptionsButton ? "left" : "center";
 
   return (
     <Card
       sx={{
         width: "328px",
-        height: "Hug(400px)",
         padding: "16px 0",
         gap: "16px",
-        borderRadius: "16px",
+        borderRadius,
+        position: "relative",
       }}
     >
-      <Typography
-        gutterBottom
-        variant="h5"
-        component="div"
-        sx={{
-          fontSize: "18px",
-          fontWeight: 600,
-          lineHeight: "25px",
-          textAlign: "center",
+      <div
+        style={{
+          width: "304px",
+          margin: "0 auto",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        {title}
-      </Typography>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{
+            fontSize: "18px",
+            fontWeight: 600,
+            lineHeight: "25px",
+            textAlign: titleAlignment,
+            flex: 1,
+          }}
+        >
+          {title}
+        </Typography>
+        {shouldShowMoreOptionsButton && (
+          <>
+            <IconButton
+              sx={{
+                position: "absolute",
+                top: 8,
+                right: 8,
+                backgroundColor: anchorEl ? "#4E169D" : "transparent", // Cambia color si está seleccionado
+                color: anchorEl ? "#FFFFFF" : "inherit", // Cambia el color del ícono si está seleccionado
+                "&:hover": {
+                  backgroundColor: "#4E169D", // Asegura que mantenga el color en hover si está seleccionado
+                },
+              }}
+              onClick={handleMenuClick}
+            >
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              sx={{ minHeight: "auto", padding: "0px", borderRadius: "0px" }}
+            >
+              <MenuItem
+                onClick={handleMenuClose}
+                sx={{
+                  width: "120px",
+                  height: "40px",
+                  fontSize: "16px",
+                  fontWeight: 400,
+                  lineHeight: "24px",
+                  textAlign: "left",
+                  padding: "0px 0px 0px 16px",
+                  minHeight: "auto",
+                  borderRadius: "0px",
+                }}
+              >
+                Editar
+              </MenuItem>
+              <MenuItem
+                onClick={handleMenuClose}
+                sx={{
+                  width: "120px",
+                  height: "40px",
+                  fontSize: "16px",
+                  fontWeight: 400,
+                  lineHeight: "24px",
+                  textAlign: "left",
+                  padding: "0px 0px 0px 16px",
+                  minHeight: "auto",
+                  borderRadius: "0px",
+                }}
+              >
+                Ocultar
+              </MenuItem>
+            </Menu>
+          </>
+        )}
+      </div>
       <Carrusel imageUrls={imageUrls} borderRadius={borderRadius} />
       <CardContent>
         <Typography
@@ -73,14 +173,15 @@ const Publication: React.FC<PublicationProps> = ({
             fontWeight: 400,
             lineHeight: "20px",
             whiteSpace: "pre-line",
+            cursor: "pointer", // Agrega el cursor de pointer para indicar que el texto es clicable
           }}
+          onClick={handleToggleText} // Añade el onClick para alternar el texto
         >
           {showFullText ? fullText : shortText}
         </Typography>
       </CardContent>
       <CardActions sx={{ justifyContent: "center" }}>
         <Button
-          // size="small"
           sx={{
             fontSize: "16px",
             fontWeight: 500,
--- src/pages/publicationsformPage.tsx
+++ src/pages/publicationsformPage.tsx
@@ -1,11 +1,80 @@
 import React from "react";
-import WorkinProgress from "../components/WorkinProgress";
+import { Typography, Container, Grid, Button } from "@mui/material";
+import Publication from "../components/Publication";
 
 const PublicationsFormPage: React.FC = () => {
   return (
-    <div>
-      <WorkinProgress />
-    </div>
+    <Container sx={{ width: "100%", paddingTop: "96px" }}>
+      <Typography
+        sx={{
+          width: "100%",
+          fontSize: "28px",
+          fontWeight: 600,
+          lineHeight: "35px",
+          textAlign: "center",
+        }}
+      >
+        Publicaciones
+      </Typography>
+      <Button
+        variant="contained"
+        disableElevation
+        sx={{
+          width: "328px",
+          backgroundColor: "#4E169D",
+          color: "#fafafa",
+          borderRadius: "100px",
+          textTransform: "none",
+          fontSize: "16px",
+          fontWeight: 700,
+          lineHeight: "30px",
+          padding: "5px 24px",
+          display: "flex",
+          alignItems: "center",
+          marginTop: 3,
+          marginBottom: 5,
+        }}
+        // onClick={handleButtonClick}
+      >
+        Crear publicación
+      </Button>
+      <Typography
+        sx={{
+          width: "100%",
+          fontSize: "22px",
+          fontWeight: 600,
+          lineHeight: "30px",
+          textAlign: "center",
+          marginBottom: 4,
+        }}
+      >
+        Publicaciones cargadas
+      </Typography>
+      <Grid container spacing={2} sx={{ position: "relative", zIndex: 1 }}>
+        {[1, 2, 3].map((index) => (
+          <Grid item xs={12} sm={12} md={6} lg={4} key={index}>
+            <Publication
+              title="¿Qué es el Upcycling?"
+              imageUrls={[
+                "/images/Publicacion upcycling imagen 1.png",
+                "/images/Publicacion upcycling 2.png",
+                "/images/Publicacion upcycling imagen 3.png",
+              ]}
+              date="17/04/2023"
+              text={`El upcycling, también conocido como supra-reciclaje o reutilización creativa, es un enfoque innovador y sostenible para la gestión de residuos y la conservación de recursos. A diferencia del reciclaje convencional, que implica descomponer materiales para crear nuevos productos, el upcycling busca transformar objetos o materiales desechados en productos de mayor valor, sin degradar su calidad.
+    
+                            Este proceso implica la reimaginación y reinvención de elementos que normalmente se considerarían basura, dándoles una segunda vida y reduciendo la cantidad de desechos enviados a vertederos. El upcycling fomenta la creatividad y la innovación, ya que requiere repensar cómo se pueden utilizar los materiales existentes de nuevas formas.
+    
+                            El upcycling se ha convertido en una poderosa herramienta para abordar los desafíos medioambientales y sociales que enfrenta nuestro planeta. Algunos ejemplos de upcycling incluyen la creación de muebles a partir de palets de madera, la confección de ropa a partir de telas recicladas o la transformación de objetos cotidianos en piezas de arte. Esto no solo reduce la cantidad de residuos, sino que también fomenta la economía circular, donde los productos y materiales se reutilizan y reciclan continuamente en lugar de desecharse.
+    
+                            El upcycling no solo beneficia al medio ambiente al reducir la cantidad de residuos, sino que también puede generar oportunidades económicas y sociales. Muchos emprendedores y artistas han encontrado en el upcycling una forma de crear productos únicos y sostenibles que atraen a consumidores conscientes de su impacto en el medio ambiente.
+    
+                            En resumen, el upcycling es una práctica innovadora que transforma desechos en tesoros, promoviendo la sostenibilidad, la creatividad y la reducción de residuos. Al adoptar el upcycling en nuestras vidas y comunidades, podemos contribuir a un mundo más limpio y respetuoso con los recursos naturales. ¡Únete al movimiento del upcycling y ayúdanos a crear un futuro más sostenible!`}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
